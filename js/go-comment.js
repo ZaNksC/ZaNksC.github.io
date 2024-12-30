fd68a992d12a6b3e48741b485b00981d8c974a58
ZaNksC <ZaNksC@users.noreply.github.com>
2024-12-30 07:50:49 +0000

    Merge pull request #2 from ZaNksC/dev
    右边侧栏+主页最新发布功能 ZaNksC/zanks-blog@d613a8ae23a18a2094e871bdb9a6591f69d33a02

---
+++ js/go-comment.js
@@ -0,0 +1,15 @@
+document.addEventListener('scroll', function () {
+    const comment = document.getElementById("comments-title");
+    const bottomToComment = document.getElementById("comments-link")
+    const viewPortHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+    const offsetTop = comment.offsetTop
+    const scrollTop = document.documentElement.scrollTop
+    const top = offsetTop - scrollTop
+    if (top <= viewPortHeight + 100) {
+        bottomToComment.style.visibility = "hidden";
+        bottomToComment.style.opacity = "0";
+    } else {
+        bottomToComment.style.visibility = "visible";
+        bottomToComment.style.opacity = "1";
+    }
+})
