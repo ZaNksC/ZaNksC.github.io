e1695b8b81736fe4d4b5359b83448041126d557f
ZaNksC <ZaNksC@users.noreply.github.com>
2024-12-31 01:39:09 +0000

    Merge pull request #4 from ZaNksC/dev
    代码与文本样式修改 ZaNksC/zanks-blog@6050125cf8de20d06e62df7ac0bb62cc66f7d282

---
+++ js/pe-base.js
@@ -0,0 +1,30 @@
+//  折叠
+let peDetails = document.getElementsByClassName('pe-details')
+for (let element of peDetails) {
+    const summary = element.getElementsByClassName('pe-details-summary')[0];
+    if (summary) {
+        summary.addEventListener('click', () => {
+            element.classList.toggle('open');
+        }, false);
+    }
+}
+
+// 链接跳转
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        const id = this.getAttribute("href").substr(1);
+        if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+            document.querySelector(`[id='${decodeURIComponent(id)}']`).scrollIntoView({
+                behavior: "smooth"
+            });
+        } else {
+            document.querySelector(`[id='${decodeURIComponent(id)}']`).scrollIntoView();
+        }
+        if (id === "top") {
+            history.replaceState(null, null, " ");
+        } else {
+            history.pushState(null, null, `#${id}`);
+        }
+    });
+});
